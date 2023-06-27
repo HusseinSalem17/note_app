2fae8f47169d311f2155481b09257386f7345127
hussein-salemm <husseinsalem177@gmail.com>
2023-06-27 17:32:09 +0300

    add note bottom sheet

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -5,17 +5,19 @@ import 'custom_note_item.dart';
 class NoteListView extends StatelessWidget {
   const NoteListView({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, idex) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4.0),
-          child: NoteItem(),
-        );
-      },
-      padding: const EdgeInsets.all(0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(
+        itemBuilder: (context, idex) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4.0),
+            child: NoteItem(),
+          );
+        },
+        padding: const EdgeInsets.all(0),
+      ),
     );
   }
 }
