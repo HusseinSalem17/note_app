d0774541815038b13f9d86268217a38e773d0cb4
hussein-salemm <husseinsalem177@gmail.com>
2023-07-01 17:37:08 +0300

    add fields to notes bottom sheet

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+
+  final String hint;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxLines,
       //change the cursor color
       cursorColor: KPrimaryColor,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         hintStyle: const TextStyle(color: KPrimaryColor),
         border: buildBorder(),
         enabledBorder: buildBorder(),
