2313d2113e9adaaa9651bfd9feccbf62ee5dd901
hussein-salemm <husseinsalem177@gmail.com>
2023-07-03 21:43:09 +0300

    add  addNote method to addNote cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,13 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
+//Hive (from flutter (package))
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-  addNote(NoteModel note){
-    
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+    //to get my box (notesBox), to use this box and add or any other operation (must detect the type of box to let his method take the same type)
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      //i can put add(note) because i made register to this type (typeAdapter)(add take dynamic type)
+      //,if didn't make registerAdapter to (noteModel) will get error
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errMessage: e.toString()));
+    }
   }
 }
